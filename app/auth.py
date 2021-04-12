e982493169b7780fd504be1d74375dda749eb0db
Richard Halford <richardh.1995@googlemail.com>
2021-04-12 15:11:01 +0100

    🔐 added simpler authentication removing check_auth function as there was no need

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,12 @@
-from flask import Response, request
+from flask import make_response, request
 from functools import wraps
-from api_access import ACCESS_TOKEN
-
-def check_auth(access_token):
-    return access_token == ACCESS_TOKEN
+from api_access import USERNAME, PASSWORD
 
 def requires_auth(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         auth = request.authorization
-        if not auth.access_token or not check_auth(auth.access_token):
-            return Response('Login', 401, {'WWW-Authenticate': 'Basic realm="Login"'})
-        return f(*args, **kwargs)
+        if auth and auth.username == USERNAME and auth.password == PASSWORD:
+            return f(*args, **kwargs)
+        return make_response('Unauthorized Access', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     return wrapper
